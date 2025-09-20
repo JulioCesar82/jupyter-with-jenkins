d7fdc081264f20fbc84243ff87117ecef5cc809a
Julio Ávila <juliocesarde.avila@yahoo.com.br>
2025-09-20 13:19:15 -0300

    Ajustes

--- conf/bin/start-tinyproxy.sh
+++ conf/bin/start-tinyproxy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Print the tinyproxy configuration to stdout
-cat <<EOF
+# Create a temporary config file for tinyproxy
+cat <<EOF > /tmp/tinyproxy.conf
 LogLevel Debug
 MaxClients 5
 MinSpareServers 5
@@ -12,3 +12,6 @@ ReverseOnly Yes
 Upstream http localhost:8080
 PreserveHostHeader Yes
 EOF
+
+# Run tinyproxy with the generated config file in the background
+tinyproxy -d -c /tmp/tinyproxy.conf &
