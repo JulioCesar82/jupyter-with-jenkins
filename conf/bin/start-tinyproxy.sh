ca940b724df5a024a1230c25c1bce9978410840c
Julio Ávila <juliocesarde.avila@yahoo.com.br>
2025-09-20 13:33:26 -0300

    Nova tentativa

--- conf/bin/start-tinyproxy.sh
+++ conf/bin/start-tinyproxy.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 
-# Create a temporary config file for tinyproxy
-cat <<EOF > /tmp/tinyproxy.conf
-LogLevel Debug
-MaxClients 5
-MinSpareServers 5
-MaxSpareServers 20
-StartServers 10
-Port 8000
-ReverseOnly Yes
-Upstream http localhost:8080
-PreserveHostHeader Yes
-EOF
-
-# Run tinyproxy with the generated config file in the background
-tinyproxy -d -c /tmp/tinyproxy.conf &
+# Run tinyproxy with the static config file
+tinyproxy -d -c /opt/jupyter-with-jenkins/conf/tinyproxy.conf &
